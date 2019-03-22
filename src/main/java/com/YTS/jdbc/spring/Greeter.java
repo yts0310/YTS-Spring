55eb1a1e5e4279bbf75cfb93bae403599ba907ac
yts0310 <yts0310@github.com>
2019-03-22 14:59:44 +0900

    Initial commit

---
+++ src/main/java/com/YTS/jdbc/spring/Greeter.java
@@ -0,0 +1,15 @@
+package com.YTS.jdbc.spring;
+
+public class Greeter {
+
+	public Greeter() {
+		System.out.println("Greeter instance create.");
+
+	}
+	
+	public String greet() {
+		return "Hello, world!";
+		
+	}
+
+}
---
+++ src/main/java/com/YTS/jdbc/spring/NomalMain.java
@@ -0,0 +1,14 @@
+package com.YTS.jdbc.spring;
+
+public class NomalMain {
+
+	public static void main(String[] args) {
+		Greeter g1 = new Greeter();
+		System.out.println(g1.greet());
+		
+		Greeter g2 = new Greeter();
+		System.out.println(g2.greet());
+
+	}
+
+}
