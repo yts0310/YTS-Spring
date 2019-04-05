5299375ba9b9d71de3dbd7e4a9852ee9d3758f4b
yts0310 <yts0310@github.com>
2019-04-05 14:04:40 +0900

    8장 내용

---
+++ src/main/java/com/YTS/jdbc/chap08/MemberService.java
@@ -0,0 +1,47 @@
+package com.YTS.jdbc.chap08;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.YTS.jdbc.chap3.Member;
+
+/**
+ * p.204 [리스트 8.13]를 Main과 Service로 분리한 Service 부분
+ * 
+ * @author Jacob
+ */
+public class MemberService {
+
+	Logger logger = LogManager.getLogger();
+
+	MemberDao memberDao = null;
+
+	/**
+	 * memberDao setter for injection
+	 */
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	public void selectAll() {
+		List<Member> members = memberDao.selectAll(0, 100);
+		logger.debug(members);
+	}
+
+	public void updateMember() {
+		Member member = memberDao.selectByEmail("sunha0822@naver.com");
+		member.setPassword("b");
+		memberDao.update(member);
+		logger.debug("Update complete.");
+	}
+
+	public void insertMember() {
+		Member member = new Member();
+		member.setEmail("yts03100310@naver.com");
+		member.setPassword("a");
+		member.setName("윤태식");
+		memberDao.insert(member);
+		logger.debug("Insert complete.");
+	}
+}
