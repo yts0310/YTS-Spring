5b83b188d8c17a4a58b5490b411dbe1642250f79
Yoon <Yoon@DESKTOP-0DF1R12>
2019-04-07 01:19:57 +0900

    article_1과제

---
+++ src/main/java/com/YTS/jdbc/Article_1/ArticleService.java
@@ -0,0 +1,77 @@
+package com.YTS.jdbc.Article_1;
+
+import java.util.List;
+import com.YTS.jdbc.Article.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * articleDao를 사용해서 글 목록, 조회, 추가, 수정, 삭제를 한다.
+ * 
+ * @author Jacob
+ */
+@Service
+public class ArticleService {
+
+	ArticleDao articleDao;
+
+	public void setArticleDao(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+
+	/**
+	 * 글 목록
+	 */
+	public void listArticles() {
+		List<Article> articles = articleDao.listArticles();
+		System.out.println(articles);
+	}
+
+	/**
+	 * 글 조회
+	 */
+	public void getArticle() {
+		Article article = articleDao.getArticle("3");
+		System.out.println(article);
+	}
+
+	/**
+	 * 글 등록
+	 */
+	public void addArticle() {
+		Article article = new Article();
+		article.setTitle("Title");
+		article.setContent(
+				"Content");
+		article.setUserId("1");
+		article.setName("윤태식");
+		if (articleDao.addArticle(article) > 0)
+			System.out.println("글을 추가했습니다.");
+		else
+			System.out.println("글을 추가하지 못했습니다.");
+	}
+
+	/**
+	 * 글 수정
+	 */
+	public void updateArticle() {
+		Article article = new Article();
+		article.setArticleId("7");
+		article.setTitle("This is modified title.");
+		article.setContent("This is modified content");
+		if (articleDao.updateArticle(article) > 0)
+			System.out.println("글을 수정했습니다.");
+		else
+			System.out.println("글을 수정하지 못했습니다.");
+	}
+
+	/**
+	 * 글 삭제
+	 */
+	public void deleteArticle() {
+		if (articleDao.deleteArticle("8") > 0)
+			System.out.println("글을 삭제했습니다.");
+		else
+			System.out.println("글을 삭제하지 못했습니다.");
+	}
+}
